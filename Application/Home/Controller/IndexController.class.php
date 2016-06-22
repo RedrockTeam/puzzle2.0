fbad2e1681d7ef151390a2efa00e9b7534f2d935
方灯 <yanghui.wyh@alibaba-inc.com>
2016-06-22 10:14:28 +0800

    为毛没效果

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -5,23 +5,11 @@ class IndexController extends Controller {
 
     private $time;
     private $code;
+    private $stuId;
     private $openid;
     private $string;
     private $secret;
     private $jsapi_ticket;
-
-    // public function score() {
-
-    //     $Score = M('score');
-
-    //     $data = M('score')->select();
-
-    //     foreach ($data as $index => $game) {
-    //     }
-    //     echo "<pre>";
-    //     print_r($this->_arraySort($data, 'spend_time'));
-    //     echo "</pre>";
-    // }
 
     public function index(){
 
@@ -49,7 +37,156 @@ class IndexController extends Controller {
         $this->display();
     }
 
-    public function getRank() {
+
+
+    //jssdk-config
+    public function JSSDKSignature(){
+        $string = $this->string;
+        $data['jsapi_ticket'] = $this->jsapi_ticket;
+        $data['noncestr'] = $this->string;
+        $data['timestamp'] = $this->time;
+        $data['url'] = 'http://'.$_SERVER['HTTP_HOST'].__SELF__;//生成当前页面url
+        $data['signature'] = sha1($this->ToUrlParams($data));
+        return $data;
+    }
+
+    private function ToUrlParams($urlObj){
+        $buff = "";
+        foreach ($urlObj as $k => $v) {
+            if($k != "signature") {
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+        $buff = trim($buff, "&");
+        return $buff;
+    }
+
+    //时间戳；签名；随机数
+    private function info() {
+        $this->time = time();
+        $str = 'abcdefghijklnmopqrstwvuxyz1234567890ABCDEFGHIJKLNMOPQRSTWVUXYZ';
+        $this->string = '';
+        for ($i = 0; $i < 16; $i++) {
+            $num = mt_rand(0,61);
+            $this->string .= $str[$num];
+        }
+        $this->secret = sha1(sha1($this->time).md5($this->string)."redrock");
+    }
+
+    //判断关注
+    private function getVerify(){
+        $t = array(
+            'string' => $this->string,
+            'token' => 'gh_68f0a1ffc303',
+            'timestamp' => $this->time,
+            'secret' => $this->secret,
+            'openid' => $this->openid,
+        );
+        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/openidVerify";
+        $result = $this->curl_api($url, $t);
+        if ($result->info == 'subscribe') {
+            session('verify', $result->info);
+        }else{
+            $this->error('没有关注小帮手');
+        }
+    }
+    
+
+    //username获取
+    private function getName(){
+        $t = array(
+            'string' => $this->string,
+            'token' => 'gh_68f0a1ffc303',
+            'timestamp' => $this->time,
+            'secret' => $this->secret,
+            'openid' => $this->openid,
+        );
+        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/userInfo";
+        $result = $this->curl_api($url, $t);
+        session('username', $result->data->nickname);
+    }
+   
+    private function getTicket(){
+        $t = array(
+            'string' => $this->string,
+            'token' => 'gh_68f0a1ffc303',
+            'timestamp' => $this->time,
+            'secret' => $this->secret,
+            'openid' => $this->openid,
+        );
+        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/apiJsTicket";
+        $result = $this->curl_api($url, $t);
+        $this->jsapi_ticket = $result->data;
+    }
+
+    //获取学号
+    private function getStuid(){
+        $t = array(
+            'string' => $this->string,
+            'token' => 'gh_68f0a1ffc303',
+            'timestamp' => $this->time,
+            'secret' => $this->secret,
+            'openid' => $this->openid,
+        );
+        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/bindVerify";
+        $result = $this->curl_api($url, $t);
+        $this->stuId = $result->stuId;
+        // return $result;
+        // if ($result->stuId) {
+        //     session('stuId', $result->stuId);
+        // }else{
+        //     $this->error('没有绑定小帮手');
+        // }
+    }
+
+    private function getOpenid(){
+        $t = array(
+            'string' => $this->string,
+            'token' => 'gh_68f0a1ffc303',
+            'timestamp' => $this->time,
+            'secret' => $this->secret,
+            'code' => $this->code,
+        );
+        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/webOAuth";
+        $result = $this->curl_api($url, $t);
+        $this->openid = $result->data->openid;
+    }
+
+    /*curl通用函数*/
+    private function curl_api($url, $data=''){
+        // 初始化一个curl对象
+        $ch = curl_init();
+        curl_setopt ( $ch, CURLOPT_URL, $url );
+        curl_setopt ( $ch, CURLOPT_POST, 1 );
+        curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
+        // 运行curl，获取网页。
+        $contents = json_decode(curl_exec($ch));
+        // 关闭请求
+        curl_close($ch);
+        return $contents;
+    }
+
+
+
+    private function _arraySort ($array, $key, $order = "asc"){ //asc是升序 desc是降序
+        $arr_nums = $arr = array();
+        foreach($array as $k => $v){
+            $arr_nums[$k] = $v[$key];
+        }
+        if ($order == 'asc'){
+            asort($arr_nums);
+        }else{
+            arsort($arr_nums);
+        }
+        foreach($arr_nums as $k => $v){
+            $arr[$k]=$array[$k];
+        }
+        return $arr;
+    }
+
+     public function getRank() {
 
         $Score = M('score');
 
@@ -91,156 +228,13 @@ class IndexController extends Controller {
                 'spend_time' => $spendTime
             ));
         }
-    	$this->ajaxReturn(array(
-    		"status" => 200,
-    		"info" => 'ok',
-    		"data" => array(
+        $this->ajaxReturn(array(
+            "status" => 200,
+            "info" => 'ok',
+            "data" => array(
                 'rank' => (string)$thisRank
             )
-    	));
-    }
-
-
-    //jssdk-config
-    public function JSSDKSignature(){
-        $string = $this->string;
-        $data['jsapi_ticket'] = $this->jsapi_ticket;
-        $data['noncestr'] = $this->string;
-        $data['timestamp'] = $this->time;
-        $data['url'] = 'http://'.$_SERVER['HTTP_HOST'].__SELF__;//生成当前页面url
-        $data['signature'] = sha1($this->ToUrlParams($data));
-        return $data;
-    }
-
-    private function ToUrlParams($urlObj){
-        $buff = "";
-        foreach ($urlObj as $k => $v) {
-            if($k != "signature") {
-                $buff .= $k . "=" . $v . "&";
-            }
-        }
-        $buff = trim($buff, "&");
-        return $buff;
-    }
-
-    //时间戳；签名；随机数
-    private function info() {
-        $this->time = time();
-        $str = 'abcdefghijklnmopqrstwvuxyz1234567890ABCDEFGHIJKLNMOPQRSTWVUXYZ';
-        $this->string = '';
-        for($i = 0; $i < 16; $i++){
-            $num = mt_rand(0,61);
-            $this->string .= $str[$num];
-        }
-        $this->secret = sha1(sha1($this->time).md5($this->string)."redrock");
-    }
-
-    //判断关注
-    private function getVerify(){
-        $t = array(
-            'string' => $this->string,
-            'token' => 'gh_68f0a1ffc303',
-            'timestamp' => $this->time,
-            'secret' => $this->secret,
-            'openid' => $this->openid,
-        );
-        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/openidVerify";
-        $result = $this->curl_api($url, $t);
-        if ($result->info == 'subscribe') {
-            session('verify', $result->info);
-        }else{
-            $this->error('没有关注小帮手');
-        }
-    }
-    //获取学号
-    private function getStuid(){
-        $t = array(
-            'string' => $this->string,
-            'token' => 'gh_68f0a1ffc303',
-            'timestamp' => $this->time,
-            'secret' => $this->secret,
-            'openid' => $this->openid,
-        );
-        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/bindVerify";
-        $result = $this->curl_api($url, $t);
-        return $result;
-        // if ($result->stuId) {
-        //     session('stuId', $result->stuId);
-        // }else{
-        //     $this->error('没有绑定小帮手');
-        // }
-    }
-
-    //username获取
-    private function getName(){
-        $t = array(
-            'string' => $this->string,
-            'token' => 'gh_68f0a1ffc303',
-            'timestamp' => $this->time,
-            'secret' => $this->secret,
-            'openid' => $this->openid,
-        );
-        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/userInfo";
-        $result = $this->curl_api($url, $t);
-        session('username', $result->data->nickname);
-    }
-   
-    private function getTicket(){
-        $t = array(
-        'string' => $this->string,
-        'token' => 'gh_68f0a1ffc303',
-        'timestamp' => $this->time,
-        'secret' => $this->secret,
-        'openid' => $this->openid,
-        );
-        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/apiJsTicket";
-        $result = $this->curl_api($url, $t);
-        $this->jsapi_ticket = $result->data;
-    }
-
-    private function getOpenid(){
-        $t = array(
-            'string' => $this->string,
-            'token' => 'gh_68f0a1ffc303',
-            'timestamp' => $this->time,
-            'secret' => $this->secret,
-            'code' => $this->code,
-        );
-        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/webOAuth";
-        $result = $this->curl_api($url, $t);
-        $this->openid = $result->data->openid;
-    }
-
-    /*curl通用函数*/
-    private function curl_api($url, $data=''){
-        // 初始化一个curl对象
-        $ch = curl_init();
-        curl_setopt ( $ch, CURLOPT_URL, $url );
-        curl_setopt ( $ch, CURLOPT_POST, 1 );
-        curl_setopt ( $ch, CURLOPT_HEADER, 0 );
-        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
-        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
-        // 运行curl，获取网页。
-        $contents = json_decode(curl_exec($ch));
-        // 关闭请求
-        curl_close($ch);
-        return $contents;
-    }
-
-    private function _arraySort ($array, $key, $order = "asc"){ //asc是升序 desc是降序
-        $arr_nums = $arr = array();
-        foreach($array as $k => $v){
-            $arr_nums[$k] = $v[$key];
-        }
-        if ($order == 'asc'){
-            asort($arr_nums);
-        }else{
-            arsort($arr_nums);
-        }
-        foreach($arr_nums as $k => $v){
-            $arr[$k]=$array[$k];
-        }
-        return $arr;
+        ));
     }
 
 
@@ -269,7 +263,7 @@ class IndexController extends Controller {
             "data" => array(
                 'list' => $data,
                 'openid' => $openid,
-                'face' => $this->getStuid()
+                'face' => $this->stuId
             )
         ));
 
